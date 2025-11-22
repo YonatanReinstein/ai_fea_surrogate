fix nans gradient in gnn

utils/gnn_surrogate.py
@@ -2,40 +2,24 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import EdgeConv, global_max_pool
 from torch_geometric.nn.models import MLP
-
+from torch_geometric.nn import BatchNorm
+import torch
 
 
 class GNN(nn.Module):
     def __init__(self, node_in_dim, hidden_dim=64, num_layers=6):
         super().__init__()
-
-        # Node encoder
-        self.encoder = MLP([node_in_dim, hidden_dim, hidden_dim])
-
-        # Build EdgeConv layers
+        self.encoder = MLP([node_in_dim, hidden_dim, hidden_dim],norm=None)
         self.convs = nn.ModuleList()
         for _ in range(num_layers):
-            self.convs.append(
-                EdgeConv(
-                    MLP([2*hidden_dim + 3, hidden_dim, hidden_dim])  # includes (x_i - x_j)
-                )
-            )
+            self.convs.append(EdgeConv(MLP([2*hidden_dim, hidden_dim, hidden_dim], norm=None), aggr="mean"))
+        self.head = MLP([hidden_dim, hidden_dim, 1], norm=None)
 
-        # Output head
-        self.head = MLP([hidden_dim, hidden_dim, 1])
-
-    def forward(self, data):
-        x, edge_index, pos, batch = data.x, data.edge_index, data.pos, data.batch
-
-        # Append relative geometry into node feature space
+    def forward(self, x, edge_index, batch):
         h = self.encoder(x)
-
-        # 6-layer EdgeConv message passing
         for conv in self.convs:
             h = conv(h, edge_index)
+        node_pred = self.head(h)
+        graph_pred = global_max_pool(node_pred, batch)
+        return graph_pred
 
-        # Global pooling → predict max stress
-        h_global = global_max_pool(h, batch)
-        out = self.head(h_global)
-
-        return out